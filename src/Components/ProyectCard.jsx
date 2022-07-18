ad332b835924e9d14e14c73f9afd58918fe826db
isaac <isaac100081@hotmail.com>
2022-07-18 00:15:16 -0500

    Resposive 4.0

--- src/Components/ProyectCard.jsx
+++ src/Components/ProyectCard.jsx
@@ -1,24 +1,34 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
-import styles from '../Styles/Style.module.css'
+import styles from "../Styles/Style.module.css";
 
-
-const ProyectCard = ({ img, title, description, proyecto, link}) => {
+const ProyectCard = ({ img, title, description, proyecto, link }) => {
   return (
     <div>
       <Card className={styles.card}>
-        <Card.Img variant="top" src={img}  />
+        <Card.Img variant="top" src={img} />
         <Card.Body>
           <Card.Title className="text-center">{title}</Card.Title>
-          <Card.Text>
-            {description}
-          </Card.Text>
-          <div className="d-flex justify-content-around">
-          <Button className="btn  w-50 " href={proyecto} style={{ backgroundColor: "#114358" }} target="_blank">Codigo</Button>
-           <Button className="btn  w-50 " href={link} style={{ backgroundColor: "#114358" }} target="_blank">Despliegue</Button>
-          </div>
-          
+          <Card.Text>{description}</Card.Text>
         </Card.Body>
+        <div className="p-2 d-flex justify-content-around">
+          <Button
+            className="btn  "
+            href={proyecto}
+            style={{ backgroundColor: "#114358", width: "35%" }}
+            target="_blank"
+          >
+            Codigo
+          </Button>
+          <Button
+            className=" "
+            href={link}
+            style={{ backgroundColor: "#114358", width: "35%" }}
+            target="_blank"
+          >
+            Despliegue
+          </Button>
+        </div>
       </Card>
     </div>
   );
